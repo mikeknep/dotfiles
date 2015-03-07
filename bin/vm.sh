194a135134b50652b95f0fbb34d339d782775902
Mike Knepper <michaelrknepper@gmail.com>
2015-03-07 14:24:09 -0600

    VM script installs plugins silently in ex mode for better display during vagrant up

--- bin/vm.sh
+++ bin/vm.sh
@@ -10,4 +10,4 @@ do
 done
 
 git clone https://github.com/gmarik/Vundle.vim.git $VM_HOME/.vim/bundle/Vundle.vim
-su vagrant -c 'sudo vim -u ~/dotfiles/vim/bundles.vim +PluginInstall +qall'
+su vagrant -c 'sudo vim -u ~/dotfiles/vim/bundles.vim +PluginInstall +qall -e -s'
