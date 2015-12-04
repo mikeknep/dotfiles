a6acaeb3350110d82e9f488071ecf9c7c44c4367
Mike Knepper <michaelrknepper@gmail.com>
2015-12-04 14:20:18 -0600

    Improve prompt colors

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -5,14 +5,16 @@ alias vcbranch="vcprompt -f '%b'"
 alias vcinfo="vcprompt -f '%m%u%a'"
 
 
-## Set readable grey color based on background color
+## Set readable colors based on background color
 if [ "${ITERM_PROFILE,,}" = 'devdark' ]
 then
   USERHOST_COLOR="${LIGHT_GRAY}"
+  GIT_COLOR="${YELLOW}"
 else
   USERHOST_COLOR="${DARK_GRAY}"
+  GIT_COLOR="${RED}"
 fi
 
 
 ## The prompt
-export PS1="\n\[$USERHOST_COLOR\](\u@\h) \[$LIGHT_BLUE\](\w) \[$YELLOW\]\$(vcbranch)\[$RED\]\$(vcinfo)\n\[$CYAN\]$ \[$RESET\]"
+export PS1="\n\[$USERHOST_COLOR\](\u@\h) \[$LIGHT_BLUE\](\w) \[$GIT_COLOR\]\$(vcbranch)\[$RED\]\$(vcinfo)\n\[$CYAN\]$ \[$RESET\]"
