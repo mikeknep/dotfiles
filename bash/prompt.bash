9f2da48535467934891118bda40c805b3a8f5aa7
Mike Knepper <michaelrknepper@gmail.com>
2016-03-23 18:23:01 -0500

    Update bash prompt colors

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -5,16 +5,22 @@ alias vcbranch="vcprompt -f '%b'"
 alias vcinfo="vcprompt -f '%m%u%a'"
 
 
-## Set readable colors based on background color
-if [ "${ITERM_PROFILE,,}" = 'devdark' ]
+## Set colors based on terminal program
+if [ $TERM_PROGRAM = 'Apple_Terminal' ]
 then
-  USERHOST_COLOR="${LIGHT_GRAY}"
-  GIT_COLOR="${YELLOW}"
-else
   USERHOST_COLOR="${DARK_GRAY}"
-  GIT_COLOR="${RED}"
+  PATH_COLOR="${DARK_GRAY}"
+  GIT_BRANCH_COLOR="${DARK_GRAY}"
+  GIT_STATUS_COLOR="${DARK_GRAY}"
+  DOLLAR_COLOR="${DARK_GRAY}"
+else
+  USERHOST_COLOR="${LIGHT_GRAY}"
+  PATH_COLOR="${LIGHT_BLUE}"
+  GIT_BRANCH_COLOR="${YELLOW}"
+  GIT_STATUS_COLOR="${RED}"
+  DOLLAR_COLOR="${CYAN}"
 fi
 
 
 ## The prompt
-export PS1="\n\[$USERHOST_COLOR\](\u@\h) \[$LIGHT_BLUE\](\w) \[$GIT_COLOR\]\$(vcbranch)\[$RED\]\$(vcinfo)\n\[$CYAN\]$ \[$RESET\]"
+export PS1="\n\[$USERHOST_COLOR\](\u@\h) \[$PATH_COLOR\](\w) \[$GIT_BRANCH_COLOR\]\$(vcbranch)\[$GIT_STATUS_COLOR\]\$(vcinfo)\n\[$DOLLAR_COLOR\]$ \[$RESET\]"
