350dc1ba0b18164fd448e77d210b3f275f6bfbb6
Mike Knepper <michaelrknepper@gmail.com>
2016-03-23 21:08:12 -0500

    Fix tmux colors and centralize

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -4,23 +4,5 @@
 alias vcbranch="vcprompt -f '%b'"
 alias vcinfo="vcprompt -f '%m%u%a'"
 
-
-## Set colors based on terminal program
-if [ $TERM_PROGRAM = 'Apple_Terminal' ]
-then
-  USERHOST_COLOR="${DARK_GRAY}"
-  PATH_COLOR="${DARK_GRAY}"
-  GIT_BRANCH_COLOR="${DARK_GRAY}"
-  GIT_STATUS_COLOR="${DARK_GRAY}"
-  DOLLAR_COLOR="${DARK_GRAY}"
-else
-  USERHOST_COLOR="${LIGHT_GRAY}"
-  PATH_COLOR="${LIGHT_BLUE}"
-  GIT_BRANCH_COLOR="${YELLOW}"
-  GIT_STATUS_COLOR="${RED}"
-  DOLLAR_COLOR="${CYAN}"
-fi
-
-
 ## The prompt
-export PS1="\n\[$USERHOST_COLOR\](\u@\h) \[$PATH_COLOR\](\w) \[$GIT_BRANCH_COLOR\]\$(vcbranch)\[$GIT_STATUS_COLOR\]\$(vcinfo)\n\[$DOLLAR_COLOR\]$ \[$RESET\]"
+export PS1="\n\[$PROMPT_USERHOST_COLOR\](\u@\h) \[$PROMPT_PATH_COLOR\](\w) \[$PROMPT_GIT_BRANCH_COLOR\]\$(vcbranch)\[$PROMPT_GIT_STATUS_COLOR\]\$(vcinfo)\n\[$PROMPT_DOLLAR_COLOR\]$ \[$PROMPT_RESET\]"
