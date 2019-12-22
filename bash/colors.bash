ae325874c5333a6c8784945b8fec7ce3c17be333
Mike Knepper <michaelrknepper@gmail.com>
2019-12-22 09:26:27 -0600

    Kill the greyscale color theme and related

--- bash/colors.bash
+++ bash/colors.bash
@@ -26,77 +26,18 @@ LIGHT_CYAN="\e[1;36m"
 RESET="\e[0m"
 
 
-# Use grayscale in Terminal and colors in iTerm
-if [ $TERM_PROGRAM = 'Apple_Terminal' ]
-then
-  export COLORSCHEME="grayscale"
-else
-  export COLORSCHEME="colors"
-fi
+export PROMPT_USERHOST_COLOR="${LIGHT_GRAY}"
+export PROMPT_PATH_COLOR="${LIGHT_BLUE}"
+export PROMPT_GIT_BRANCH_COLOR="${YELLOW}"
+export PROMPT_GIT_STATUS_COLOR="${RED}"
+export PROMPT_DOLLAR_COLOR="${CYAN}"
+export PROMPT_RESET="${RESET}"
+export TMUX_BORDER_COLOR="#585858"
+export TMUX_STATUS_TEXT_COLOR="#8787af"
+export TMUX_STATUS_BACKGROUND_COLOR="#262626"
+export TMUX_ALERT_TEXT_COLOR="#dadada"
+export TMUX_ALERT_BACKGROUND_COLOR="#303030"
+export TMUX_ACTIVE_WINDOW_TEXT_COLOR="#dadada"
+export TMUX_ACTIVE_WINDOW_BACKGROUND_COLOR="#262626"
 
-
-if [ $COLORSCHEME = "grayscale" ]
-then
-  export PROMPT_USERHOST_COLOR="${DARK_GRAY}"
-  export PROMPT_PATH_COLOR="${DARK_GRAY}"
-  export PROMPT_GIT_BRANCH_COLOR="${DARK_GRAY}"
-  export PROMPT_GIT_STATUS_COLOR="${DARK_GRAY}"
-  export PROMPT_DOLLAR_COLOR="${DARK_GRAY}"
-  export PROMPT_RESET="${RESET}"
-  export TMUX_BORDER_COLOR="#585858"
-  export TMUX_STATUS_TEXT_COLOR="#303030"
-  export TMUX_STATUS_BACKGROUND_COLOR="#f5f6f6"
-  export TMUX_ALERT_TEXT_COLOR="#080808"
-  export TMUX_ALERT_BACKGROUND_COLOR="#dadada"
-  export TMUX_ACTIVE_WINDOW_TEXT_COLOR="#dadada"
-  export TMUX_ACTIVE_WINDOW_BACKGROUND_COLOR="#8a8a8a"
-else
-  export PROMPT_USERHOST_COLOR="${LIGHT_GRAY}"
-  export PROMPT_PATH_COLOR="${LIGHT_BLUE}"
-  export PROMPT_GIT_BRANCH_COLOR="${YELLOW}"
-  export PROMPT_GIT_STATUS_COLOR="${RED}"
-  export PROMPT_DOLLAR_COLOR="${CYAN}"
-  export PROMPT_RESET="${RESET}"
-  export TMUX_BORDER_COLOR="#585858"
-  export TMUX_STATUS_TEXT_COLOR="#8787af"
-  export TMUX_STATUS_BACKGROUND_COLOR="#262626"
-  export TMUX_ALERT_TEXT_COLOR="#dadada"
-  export TMUX_ALERT_BACKGROUND_COLOR="#303030"
-  export TMUX_ACTIVE_WINDOW_TEXT_COLOR="#dadada"
-  export TMUX_ACTIVE_WINDOW_BACKGROUND_COLOR="#262626"
-fi
-
-
-
-##
-## Color adjustment for iTerm2 (works better on/off depending on specific colorscheme)
-## More info: https://github.com/morhetz/gruvbox/issues/8#issuecomment-27627656
-##
-# echo -ne "\x1bP\x1b]4;236;rgb:32/30/2f\a\x1b\\"
-# echo -ne "\x1bP\x1b]4;234;rgb:1d/20/21\a\x1b\\"
-# echo -ne "\x1bP\x1b]4;235;rgb:20/20/20\a\x1b\\"
-# echo -ne "\x1bP\x1b]4;237;rgb:3c/38/36\a\x1b\\"
-# echo -ne "\x1bP\x1b]4;239;rgb:50/49/45\a\x1b\\"
-# echo -ne "\x1bP\x1b]4;241;rgb:66/5c/54\a\x1b\\"
-# echo -ne "\x1bP\x1b]4;243;rgb:7c/6f/64\a\x1b\\"
-# echo -ne "\x1bP\x1b]4;244;rgb:92/83/74\a\x1b\\"
-# echo -ne "\x1bP\x1b]4;245;rgb:92/83/74\a\x1b\\"
-# echo -ne "\x1bP\x1b]4;229;rgb:fd/f4/c1\a\x1b\\"
-# echo -ne "\x1bP\x1b]4;223;rgb:eb/db/b2\a\x1b\\"
-# echo -ne "\x1bP\x1b]4;250;rgb:d5/c4/a1\a\x1b\\"
-# echo -ne "\x1bP\x1b]4;248;rgb:bd/ae/93\a\x1b\\"
-# echo -ne "\x1bP\x1b]4;246;rgb:a8/99/84\a\x1b\\"
-# echo -ne "\x1bP\x1b]4;167;rgb:fb/49/34\a\x1b\\"
-# echo -ne "\x1bP\x1b]4;142;rgb:b8/bb/26\a\x1b\\"
-# echo -ne "\x1bP\x1b]4;214;rgb:fa/bd/2f\a\x1b\\"
-# echo -ne "\x1bP\x1b]4;109;rgb:83/a5/98\a\x1b\\"
-# echo -ne "\x1bP\x1b]4;175;rgb:d3/86/9b\a\x1b\\"
-# echo -ne "\x1bP\x1b]4;108;rgb:8e/c0/7c\a\x1b\\"
-# echo -ne "\x1bP\x1b]4;208;rgb:fe/80/19\a\x1b\\"
-# echo -ne "\x1bP\x1b]4;88;rgb:9d/00/06\a\x1b\\"
-# echo -ne "\x1bP\x1b]4;100;rgb:79/74/0e\a\x1b\\"
-# echo -ne "\x1bP\x1b]4;136;rgb:b5/76/14\a\x1b\\"
-# echo -ne "\x1bP\x1b]4;24;rgb:07/66/78\a\x1b\\"
-# echo -ne "\x1bP\x1b]4;96;rgb:8f/3f/71\a\x1b\\"
-# echo -ne "\x1bP\x1b]4;66;rgb:42/7b/58\a\x1b\\"
-# echo -ne "\x1bP\x1b]4;130;rgb:af/3a/03\a\x1b\\"
+# For more info on color adjustments, paricularly in iTerm2, see: https://github.com/morhetz/gruvbox/issues/8#issuecomment-27627656
--- vim/colorschemes.vim
+++ vim/colorschemes.vim
@@ -1,24 +1,4 @@
-if $COLORSCHEME == "grayscale"
-  colorscheme m
-  set background=light
-  syntax off
-else
-  colorscheme gruvbox
-  let g:gruvbox_italic=0
-  set background=dark
-  syntax on
-endif
-
-function ColorschemeSwap()
-  if g:colors_name == "m"
-    colorscheme gruvbox
-    set background=dark
-    syntax on
-  elseif g:colors_name == "gruvbox"
-    colorscheme m
-    set background=light
-    syntax off
-  endif
-endfunction
-
-map <leader>cs :call ColorschemeSwap()<CR>
+colorscheme gruvbox
+let g:gruvbox_italic=0
+set background=dark
+syntax on
