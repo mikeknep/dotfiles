d093cb35a779a707e28547bb09bc06fd014cff76
Mike Knepper <michaelrknepper@gmail.com>
2020-09-01 15:18:39 -0500

    Add functions for switching between light and dark colors

--- bash/functions.bash
+++ bash/functions.bash
@@ -38,3 +38,20 @@ bump() {
   brew upgrade
   pyenv rehash
 }
+
+
+light_mode() {
+  sed -i '' -E "s%colorscheme .*%colorscheme solarized%g" ~/dotfiles/vim/settings.vim
+  sed -i '' -E "s%set background=.*%set background=light%g" ~/dotfiles/vim/settings.vim
+  sed -i '' -E "s%colors: .+%colors: \*solarized_light%g" ~/dotfiles/.alacritty.yml
+  sed -i '' -E "s%set -g \@plugin .* # colors%set -g \@plugin 'seebi\\/tmux-colors-solarized' # colors%g" ~/dotfiles/.tmux.conf
+  tmux source ~/dotfiles/.tmux.conf
+}
+
+dark_mode() {
+  sed -i '' -E "s%colorscheme .*%colorscheme gruvbox%g" ~/dotfiles/vim/settings.vim
+  sed -i '' -E "s%set background=.*%set background=dark%g" ~/dotfiles/vim/settings.vim
+  sed -i '' -E "s%colors: .+%colors: \*gruvbox_dark%g" ~/dotfiles/.alacritty.yml
+  sed -i '' -E "s%set -g \@plugin .* # colors%set -g \@plugin 'egel\\/tmux-gruvbox' # colors%g" ~/dotfiles/.tmux.conf
+  tmux source ~/dotfiles/.tmux.conf
+}
