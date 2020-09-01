7dd1a0984dc1a8d4f2f15e8950b896c5d31d416b
Mike Knepper <michaelrknepper@gmail.com>
2020-09-01 13:21:56 -0500

    Remove userhost from prompt

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -5,4 +5,4 @@ alias vcbranch="vcprompt -f '%b'"
 alias vcinfo="vcprompt -f '%m%u%a'"
 
 ## The prompt
-export PS1="\n\[$PROMPT_USERHOST_COLOR\](\u@\h) \[$PROMPT_PATH_COLOR\](\w) \[$PROMPT_GIT_BRANCH_COLOR\]\$(vcbranch)\[$PROMPT_GIT_STATUS_COLOR\]\$(vcinfo)\n\[$PROMPT_DOLLAR_COLOR\]$ \[$PROMPT_RESET\]"
+export PS1="\n\[$PROMPT_PATH_COLOR\](\w) \[$PROMPT_GIT_BRANCH_COLOR\]\$(vcbranch)\[$PROMPT_GIT_STATUS_COLOR\]\$(vcinfo)\n\[$PROMPT_DOLLAR_COLOR\]$ \[$PROMPT_RESET\]"
