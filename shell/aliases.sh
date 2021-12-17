f1271fcdd845573b218cbd9faf97dfe1ba12a93e
Mike Knepper <github@mikeknep.com>
2021-12-17 15:19:03 -0600

    Remove some unusued aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -6,8 +6,6 @@ alias sandbox="cd $HOME/sandbox"
 
 
 ## Misc
-alias be='bundle exec'
-alias bi='bundle install'
 alias ll='ls -AFGgohl'
 
 
@@ -15,22 +13,13 @@ alias ll='ls -AFGgohl'
 alias g='git'
 alias gs='git status'
 alias gp='git push'
-alias gpf='git push --force-with-lease'
 alias gpl='git pull'
-alias ga='git add'
 alias gaa='git add -A'
 alias gc='git commit'
 alias gcm='git commit -m'
-alias gcv='git commit -v'
 alias gca='git commit --amend'
 alias grh='git reset --hard'
-alias gco='echo Use switch [-c] or restore!'
 alias gl='git lg -15'
 alias gd='git diff'
 alias gds='git diff --staged'
 alias gb='git branch'
-alias gsha='git rev-parse --short HEAD'
-
-gg() {
-  echo "Use ripgrep! rg"
-}
