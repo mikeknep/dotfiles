a212dadaa58220750074bc12d3e4c21a4fcdd6df
Mike Knepper <github@mikeknep.com>
2022-05-11 08:39:25 -0500

    Install ripgrep on codespaces

--- install.sh
+++ install.sh
@@ -9,6 +9,11 @@ curl -sS https://webinstall.dev/delta | bash
 
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 
+RIPGREP_VERSION=$(curl -s "https://api.github.com/repos/BurntSushi/ripgrep/releases/latest" | grep -Po '"tag_name": "\K[0-9.]+')
+curl -Lo ripgrep.deb "https://github.com/BurntSushi/ripgrep/releases/latest/download/ripgrep_${RIPGREP_VERSION}_amd64.deb"
+apt install -y ./ripgrep.deb
+rm -rf ripgrep.deb
+
 
 # Symlink dotfiles
 dotfiles_dir=$(dirname "$(readlink -f "$0")")
