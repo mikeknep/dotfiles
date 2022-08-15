995dcccd1238eef494028e5c603742221de1e08c
Mike Knepper <github@mikeknep.com>
2022-08-14 21:28:02 -0500

    Update neovim symlink

--- bin/symlink-all.sh
+++ bin/symlink-all.sh
@@ -4,6 +4,6 @@ ln -sf $HOME/dotfiles/.alacritty.yml $HOME/.alacritty.yml
 ln -sf $HOME/dotfiles/.gitconfig $HOME/.gitconfig
 ln -sf $HOME/dotfiles/.tmux.conf $HOME/.config/tmux/tmux.conf
 ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
-ln -sf $HOME/dotfiles/.vimrc $HOME/.config/nvim/init.vim
+ln -sf $HOME/dotfiles/neovim.lua $HOME/.config/nvim/init.lua
 ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
 ln -sf $HOME/dotfiles/starship.toml $HOME/.config/starship.toml
