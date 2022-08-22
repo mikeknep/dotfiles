4cc4b5bf189664522e846e4307cd9d852da99b48
Mike Knepper <github@mikeknep.com>
2022-08-22 08:34:06 -0500

    Add vscode settings

--- bin/symlink-all.sh
+++ bin/symlink-all.sh
@@ -6,3 +6,5 @@ ln -sf $HOME/dotfiles/.tmux.conf $HOME/.config/tmux/tmux.conf
 ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
 ln -sf $HOME/dotfiles/init.lua $HOME/.config/nvim/init.lua
 ln -sf $HOME/dotfiles/starship.toml $HOME/.config/starship.toml
+ln -sf $HOME/dotfiles/vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
+ln -sf $HOME/dotfiles/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
