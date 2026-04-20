9fabc4f997a5b9fb4000bb5cd03f6b303389a68f
Mike Knepper <git@mikeknep.com>
2026-04-20 09:09:36 -0500

    Fix render-markdown after upgrading to neovim 0.12

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,4 +1,6 @@
 require("render-markdown").setup({
+    restart_highlighter = true,
+
     -- The configuration docs are really good:
     -- https://github.com/MeanderingProgrammer/render-markdown.nvim?tab=readme-ov-file#setup
 
@@ -33,6 +35,14 @@ require("render-markdown").setup({
 
 
 local function adjust_colors()
+  local function set_hl(name, opts)
+    vim.api.nvim_set_hl(0, name, vim.tbl_extend("force", opts, { force = true }))
+  end
+
+  local function get_hl(name)
+    return vim.api.nvim_get_hl(0, { name = name, link = false })
+  end
+
   local orange_fg = "#fe8019"
   local orange_ct = 208
 
@@ -42,43 +52,72 @@ local function adjust_colors()
   local yellow_fg = "#fabd2f"
   local yellow_ct = 214
 
+  local grey_fg = "#928374"
+  local grey_ct = 245
+
   -- Headers. Set everything to orange.
   -- There are a variety of area groups to set here due to multiple, slightly-overlapping plugins
   -- (vim-polygot, render-markdown) affecting different parts of the line (#, symbol, text).
-  vim.api.nvim_set_hl(0, "mkdHeading", { fg = orange_fg, ctermfg = orange_ct, force = true })
-  vim.api.nvim_set_hl(0, "mkdHeadingDelimiter", { fg = orange_fg, ctermfg = orange_ct, force = true })
-  vim.api.nvim_set_hl(0, "htmlH1", { fg = orange_fg, ctermfg = orange_ct, force = true })
-  vim.api.nvim_set_hl(0, "htmlH2", { fg = orange_fg, ctermfg = orange_ct, force = true })
-  vim.api.nvim_set_hl(0, "htmlH3", { fg = orange_fg, ctermfg = orange_ct, force = true })
-  vim.api.nvim_set_hl(0, "htmlH4", { fg = orange_fg, ctermfg = orange_ct, force = true })
-  vim.api.nvim_set_hl(0, "htmlH5", { fg = orange_fg, ctermfg = orange_ct, force = true })
-  vim.api.nvim_set_hl(0, "htmlH6", { fg = orange_fg, ctermfg = orange_ct, force = true })
-  vim.api.nvim_set_hl(0, "RenderMarkdownH1", { fg = orange_fg, ctermfg = orange_ct, force = true })
-  vim.api.nvim_set_hl(0, "RenderMarkdownH2", { fg = orange_fg, ctermfg = orange_ct, force = true })
-  vim.api.nvim_set_hl(0, "RenderMarkdownH3", { fg = orange_fg, ctermfg = orange_ct, force = true })
-  vim.api.nvim_set_hl(0, "RenderMarkdownH4", { fg = orange_fg, ctermfg = orange_ct, force = true })
-  vim.api.nvim_set_hl(0, "RenderMarkdownH5", { fg = orange_fg, ctermfg = orange_ct, force = true })
-  vim.api.nvim_set_hl(0, "RenderMarkdownH6", { fg = orange_fg, ctermfg = orange_ct, force = true })
+  set_hl("mkdHeading", { fg = orange_fg, ctermfg = orange_ct })
+  set_hl("mkdHeadingDelimiter", { fg = orange_fg, ctermfg = orange_ct })
+  set_hl("htmlH1", { fg = orange_fg, ctermfg = orange_ct })
+  set_hl("htmlH2", { fg = orange_fg, ctermfg = orange_ct })
+  set_hl("htmlH3", { fg = orange_fg, ctermfg = orange_ct })
+  set_hl("htmlH4", { fg = orange_fg, ctermfg = orange_ct })
+  set_hl("htmlH5", { fg = orange_fg, ctermfg = orange_ct })
+  set_hl("htmlH6", { fg = orange_fg, ctermfg = orange_ct })
+  set_hl("@markup.heading", { fg = orange_fg, ctermfg = orange_ct })
+  set_hl("@markup.heading.1.markdown", { fg = orange_fg, ctermfg = orange_ct })
+  set_hl("@markup.heading.2.markdown", { fg = orange_fg, ctermfg = orange_ct })
+  set_hl("@markup.heading.3.markdown", { fg = orange_fg, ctermfg = orange_ct })
+  set_hl("@markup.heading.4.markdown", { fg = orange_fg, ctermfg = orange_ct })
+  set_hl("@markup.heading.5.markdown", { fg = orange_fg, ctermfg = orange_ct })
+  set_hl("@markup.heading.6.markdown", { fg = orange_fg, ctermfg = orange_ct })
+  set_hl("RenderMarkdownH1", { fg = orange_fg, ctermfg = orange_ct })
+  set_hl("RenderMarkdownH2", { fg = orange_fg, ctermfg = orange_ct })
+  set_hl("RenderMarkdownH3", { fg = orange_fg, ctermfg = orange_ct })
+  set_hl("RenderMarkdownH4", { fg = orange_fg, ctermfg = orange_ct })
+  set_hl("RenderMarkdownH5", { fg = orange_fg, ctermfg = orange_ct })
+  set_hl("RenderMarkdownH6", { fg = orange_fg, ctermfg = orange_ct })
 
   -- Block quotes. Set to teal.
-  vim.api.nvim_set_hl(0, "RenderMarkdownQuote1", { fg = teal_fg, ctermfg = teal_ct, force = true })
-  vim.api.nvim_set_hl(0, "RenderMarkdownQuote2", { fg = teal_fg, ctermfg = teal_ct, force = true })
-  vim.api.nvim_set_hl(0, "RenderMarkdownQuote3", { fg = teal_fg, ctermfg = teal_ct, force = true })
-  vim.api.nvim_set_hl(0, "RenderMarkdownQuote4", { fg = teal_fg, ctermfg = teal_ct, force = true })
-  vim.api.nvim_set_hl(0, "RenderMarkdownQuote5", { fg = teal_fg, ctermfg = teal_ct, force = true })
-  vim.api.nvim_set_hl(0, "RenderMarkdownQuote6", { fg = teal_fg, ctermfg = teal_ct, force = true })
+  set_hl("@markup.quote", { fg = teal_fg, ctermfg = teal_ct })
+  set_hl("@markup.quote.markdown", { fg = teal_fg, ctermfg = teal_ct })
+  set_hl("RenderMarkdownQuote", { fg = teal_fg, ctermfg = teal_ct })
+  set_hl("RenderMarkdownQuote1", { fg = teal_fg, ctermfg = teal_ct })
+  set_hl("RenderMarkdownQuote2", { fg = teal_fg, ctermfg = teal_ct })
+  set_hl("RenderMarkdownQuote3", { fg = teal_fg, ctermfg = teal_ct })
+  set_hl("RenderMarkdownQuote4", { fg = teal_fg, ctermfg = teal_ct })
+  set_hl("RenderMarkdownQuote5", { fg = teal_fg, ctermfg = teal_ct })
+  set_hl("RenderMarkdownQuote6", { fg = teal_fg, ctermfg = teal_ct })
 
   -- Bold and italics set to yellow
-  vim.api.nvim_set_hl(0, "htmlBold", { fg = yellow_fg, ctermfg = yellow_ct, force = true })
-  vim.api.nvim_set_hl(0, "htmlItalic", { fg = yellow_fg, ctermfg = yellow_ct, force = true })
-  -- TODO: strikethrough
-  -- currently double-tilde is getting interpreted as a fenced code block and thus rendering green
-  -- below are attempts to change it to one of gruvbox's greys
-  -- local grey_fg = "#928374"
-  -- local grey_ct = 245
-  -- vim.api.nvim_set_hl(0, "htmlStrike", { fg = grey_fg, ctermfg = grey_ct, force = true })
-  -- vim.api.nvim_set_hl(0, "TTStrikethrough", { fg = grey_fg, ctermfg = grey_ct, force = true })
-  -- vim.api.nvim_set_hl(0, "@markup.strikethrough", { fg = grey_fg, ctermfg = grey_ct, force = true })
+  set_hl("htmlBold", { fg = yellow_fg, ctermfg = yellow_ct })
+  set_hl("htmlItalic", { fg = yellow_fg, ctermfg = yellow_ct })
+  set_hl("@markup.strong", { fg = yellow_fg, ctermfg = yellow_ct })
+  set_hl("@markup.strong.markdown_inline", { fg = yellow_fg, ctermfg = yellow_ct })
+  set_hl("@markup.italic", { fg = yellow_fg, ctermfg = yellow_ct, italic = true, cterm = { italic = true } })
+  set_hl("@markup.italic.markdown_inline", { fg = yellow_fg, ctermfg = yellow_ct, italic = true, cterm = { italic = true } })
+
+  -- Link text should follow gruvbox's Link highlight (purple + underline).
+  set_hl("@markup.link", { link = "Link" })
+  set_hl("@markup.link.label", { link = "Link" })
+  set_hl("@markup.link.label.markdown_inline", { link = "Link" })
+  set_hl("RenderMarkdownLink", { link = "Link" })
+  set_hl("RenderMarkdownLinkTitle", { link = "Link" })
+
+  -- Neovim 0.12's markdown parser highlights fenced blocks as raw text. Keep the
+  -- render-markdown background, but avoid setting a foreground so injected syntax can show through.
+  local code_hl = get_hl("RenderMarkdownCode")
+  set_hl("@markup.raw.block", { bg = code_hl.bg, ctermbg = code_hl.ctermbg })
+  set_hl("@markup.raw.block.markdown", { bg = code_hl.bg, ctermbg = code_hl.ctermbg })
+
+  -- Strikethrough comes from terminal/editor text attributes, not from the font.
+  -- If the terminal does not render the line, the grey fallback still distinguishes the text.
+  set_hl("htmlStrike", { fg = grey_fg, ctermfg = grey_ct, strikethrough = true, cterm = { strikethrough = true } })
+  set_hl("TTStrikethrough", { fg = grey_fg, ctermfg = grey_ct, strikethrough = true, cterm = { strikethrough = true } })
+  set_hl("@markup.strikethrough", { fg = grey_fg, ctermfg = grey_ct, strikethrough = true, cterm = { strikethrough = true } })
+  set_hl("@markup.strikethrough.markdown_inline", { fg = grey_fg, ctermfg = grey_ct, strikethrough = true, cterm = { strikethrough = true } })
 end
 
 adjust_colors()
---
+++ nvim/markdown-render-sample.md
@@ -0,0 +1,30 @@
+# Heading 1
+
+## Heading 2
+
+Regular paragraph text with **bold**, *italic*, and ~~strikethrough~~.
+
+Here is a [link to github](https://github.com).
+
+> Blockquote line one
+>
+> Blockquote line two with **bold** and *italic*.
+
+Inline code looks like `let x = 1`.
+
+```lua
+local message = "lua fenced code"
+print(message)
+```
+
+```python
+def greet(name: str) -> None:
+    print(f"hello, {name}")
+```
+
+```json
+{
+  "feature": "markdown render sample",
+  "enabled": true
+}
+```
